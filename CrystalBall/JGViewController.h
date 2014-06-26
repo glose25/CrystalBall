62a37e96ba75dc9852c5a634b9f13a5328776b9e
Jason Glose <glosej@rpi.edu>
2014-06-25 20:17:00 -0400

    Initial Commit

---
+++ CrystalBall/JGViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGViewController.h
+//  CrystalBall
+//
+//  Created by Jay Glose on 6/25/14.
+//  Copyright (c) 2014 Glose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGViewController : UIViewController
+
+@end
---
+++ CrystalBall/JGViewController.m
@@ -0,0 +1,29 @@
+//
+//  JGViewController.m
+//  CrystalBall
+//
+//  Created by Jay Glose on 6/25/14.
+//  Copyright (c) 2014 Glose. All rights reserved.
+//
+
+#import "JGViewController.h"
+
+@interface JGViewController ()
+
+@end
+
+@implementation JGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
